f6f54d9718f0c063a651db7f51b91c50ca56ee3a
khowellmobile <khowellmobile@gmail.com>
2025-04-15 12:34:51 -0400

    Added basic account model endpoints

--- backend/rental_api/views.py
+++ backend/rental_api/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import TransactionSerializer
-from core_backend.models import Transaction
+from .serializers import TransactionSerializer, AccountSerializer
+from core_backend.models import Transaction, Account
 
 class TransactionListAPIView(APIView):
     """
@@ -25,4 +25,27 @@ class TransactionDetailAPIView(APIView):
         except Transaction.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = TransactionSerializer(transaction)
+        return Response(serializer.data)
+    
+
+
+class AccountListAPIView(APIView):
+    """
+    API endpoint to list all accounts.
+    """
+    def get(self, request):
+        accounts = Account.objects.all()
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data)
+
+class AccountDetailAPIView(APIView):
+    """
+    API endpoint to retrieve a single account by its primary key (id).
+    """
+    def get(self, request, pk):
+        try:
+            account = Account.objects.get(pk=pk)
+        except Account.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = AccountSerializer(account)
         return Response(serializer.data)
