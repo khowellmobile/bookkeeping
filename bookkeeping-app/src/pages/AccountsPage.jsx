2eb62a323f510fe90f8ab14dc7cdac3a48aaf9a9
khowellmobile <khowellmobile@gmail.com>
2025-05-01 14:45:43 -0400

    Attempting to fix account loading problem.

--- bookkeeping-app/src/pages/AccountsPage.jsx
+++ bookkeeping-app/src/pages/AccountsPage.jsx
@@ -1,6 +1,6 @@
 import classes from "./AccountsPage.module.css";
 import BkpgContext from "../components/contexts/BkpgContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AddAccountModal from "../components/elements/modals/AddAccountModal";
 
@@ -21,51 +21,55 @@ const AccountsPage = () => {
     };
 
     if (!ctxAccountList) {
-        return <div>Accounts not loaded</div>;
+        return <div>Accounts not loaded. Refresh Page.</div>;
     }
 
     return (
         <>
             {isModalOpen && <AddAccountModal handleCloseModal={handleCloseModal} />}
 
-            <div className={classes.mainContainer}>
-                <div className={classes.accountsHeader}>
-                    <h2>Accounts</h2>
-                    <div className={classes.tools}>
-                        <div>
-                            <input
-                                type="text"
-                                className={classes.accountSearch}
-                                placeholder="Search..."
-                                spellCheck="false"
-                            ></input>
-                        </div>
-                        <div>
-                            <button onClick={() => setIsModalOpen(true)}>Add Account</button>
-                        </div>
-                    </div>
-                </div>
-                <div className={classes.accountListing}>
-                    <div className={classes.listingHeader}>
-                        <p>Name</p>
-                        <p>Type</p>
-                        <p>Description</p>
-                        <p>Balance</p>
-                        <p>Action</p>
-                    </div>
-                    <div className={classes.listingItems}>
-                        {ctxAccountList.map((account, index) => (
-                            <div key={index}>
-                                <p onClick={() => accountClickHandler(account.name)}>{account.name}</p>
-                                <p>{account.type}</p>
-                                <p>{account.description}</p>
-                                <p>{account.balance}</p>
-                                <p>{account.initial_balance}</p>
+            {ctxIsLoading ? (
+                <div>Accounts loading</div>
+            ) : (
+                <div className={classes.mainContainer}>
+                    <div className={classes.accountsHeader}>
+                        <h2>Accounts</h2>
+                        <div className={classes.tools}>
+                            <div>
+                                <input
+                                    type="text"
+                                    className={classes.accountSearch}
+                                    placeholder="Search..."
+                                    spellCheck="false"
+                                ></input>
                             </div>
-                        ))}
+                            <div>
+                                <button onClick={() => setIsModalOpen(true)}>Add Account</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.accountListing}>
+                        <div className={classes.listingHeader}>
+                            <p>Name</p>
+                            <p>Type</p>
+                            <p>Description</p>
+                            <p>Balance</p>
+                            <p>Action</p>
+                        </div>
+                        <div className={classes.listingItems}>
+                            {ctxAccountList.map((account, index) => (
+                                <div key={index}>
+                                    <p onClick={() => accountClickHandler(account.name)}>{account.name}</p>
+                                    <p>{account.type}</p>
+                                    <p>{account.description}</p>
+                                    <p>{account.balance}</p>
+                                    <p>{account.initial_balance}</p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </>
     );
 };
