01441c4470eff5b913a3aeaf13b52344f0f7fd3f
khowellmobile <khowellmobile@gmail.com>
2025-05-05 15:28:31 -0400

    Added account actions and actions dropdown

--- bookkeeping-app/src/components/elements/items/AccountItem.jsx
+++ bookkeeping-app/src/components/elements/items/AccountItem.jsx
@@ -1,9 +1,9 @@
 import classes from "./AccountItem.module.css";
-
 import BkpgContext from "../../contexts/BkpgContext";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import AccountModal from "../modals/AccountModal";
+import ThreeDotsIcon from "../../../assets/three-dots-icon.svg";
 
 const AccountItem = ({ account }) => {
     const navigate = useNavigate();
@@ -11,6 +11,9 @@ const AccountItem = ({ account }) => {
     const { changeCtxActiveAccount } = useContext(BkpgContext);
 
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isDropOpen, setIsDropOpen] = useState(false);
+
+    const dropdownRef = useRef(null);
 
     const accountClickHandler = (account) => {
         changeCtxActiveAccount(account);
@@ -21,16 +24,49 @@ const AccountItem = ({ account }) => {
         setIsModalOpen(false);
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isDropOpen && dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsDropOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isDropOpen, dropdownRef]);
+
     return (
         <>
-            {isModalOpen && <AccountModal account={account} handleCloseModal={handleCloseModal}/>}
+            {isModalOpen && <AccountModal account={account} handleCloseModal={handleCloseModal} />}
 
-            <div className={classes.mainContainer} onClick={() => setIsModalOpen(true)}>
-                <p onClick={() => accountClickHandler(account.name)}>{account.name}</p>
+            <div className={classes.mainContainer}>
+                <p>{account.name}</p>
                 <p>{account.type}</p>
                 <p>{account.description}</p>
                 <p>{account.balance}</p>
-                <p>{account.initial_balance}</p>
+                <div className={classes.actionDiv}>
+                    <img
+                        src={ThreeDotsIcon}
+                        className={classes.icon}
+                        alt="Icon"
+                        onClick={() => setIsDropOpen(!isDropOpen)}
+                    />
+                    {isDropOpen && (
+                        <div className={classes.actionDropdown} ref={dropdownRef}>
+                            <div onClick={() => accountClickHandler(account.name)}>
+                                <p>Go to Transactions</p>
+                            </div>
+                            <div onClick={() => setIsModalOpen(true)}>
+                                <p>Edit</p>
+                            </div>
+                            <div>
+                                <p>Delete</p>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     );
