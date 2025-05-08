7c46fb93b2e2c1df598e7e1544deb380c2c69186
khowellmobile <khowellmobile@gmail.com>
2025-05-08 13:38:36 -0400

    Added dropdown overflow fix to EntityDropdown

--- bookkeeping-app/src/components/elements/dropdowns/EntityEntryDropdown.jsx
+++ bookkeeping-app/src/components/elements/dropdowns/EntityEntryDropdown.jsx
@@ -4,14 +4,52 @@ import BkpgContext from "../../contexts/BkpgContext";
 
 import { useState, useContext, useEffect, useRef } from "react";
 
-const EntityEntryDropdown = ({ onChange }) => {
+const EntityEntryDropdown = ({ scrollRef, onChange }) => {
     const { ctxEntityList } = useContext(BkpgContext);
 
     const [searchTerm, setSearchTerm] = useState("");
     const [filteredEntitys, setFilteredEntitys] = useState(ctxEntityList);
     const [isExpanded, setIsExpanded] = useState(false);
 
+    const [isOffScreenBottom, setIsOffScreenBottom] = useState();
+    const [pxScroll, setPxScroll] = useState(0);
+
     const inputRef = useRef();
+
+    let style = isOffScreenBottom
+        ? { bottom: `calc(1.5rem + ${pxScroll}px + 1px)` }
+        : { top: `calc(1.5rem - ${pxScroll}px)` };
+
+    const checkOffScreen = () => {
+        try {
+            const r = inputRef.current;
+
+            if (r) {
+                const rectR = r.getBoundingClientRect();
+                const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+                const isOffScreenBottom = rectR.bottom + 320 > windowHeight;
+                setIsOffScreenBottom(isOffScreenBottom);
+            }
+        } catch (error) {
+            console.log(error, "Safe to ignore");
+        }
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (scrollRef.current) {
+                setIsExpanded(false);
+                setPxScroll(scrollRef.current.scrollTop);
+            } else {
+                console.log("scrollRef.current is null");
+            }
+        };
+
+        const scrollableDiv = scrollRef.current;
+        if (scrollableDiv) {
+            scrollableDiv.addEventListener("scroll", handleScroll);
+        }
+    }, []);
 
     useEffect(() => {
         if (ctxEntityList) {
@@ -36,7 +74,7 @@ const EntityEntryDropdown = ({ onChange }) => {
     };
 
     return (
-        <div className={classes.mainContainer}>
+        <div className={classes.mainContainer} onFocus={() => checkOffScreen()}>
             <input
                 type="text"
                 value={searchTerm}
@@ -48,25 +86,23 @@ const EntityEntryDropdown = ({ onChange }) => {
                 onBlur={handleBlur}
                 ref={inputRef}
             />
-            {isExpanded && (
-                <div className={classes.anchor}>
-                    <div className={classes.dropDownContent}>
-                        <p>All entitys</p>
-                        <div className={classes.separatorH}></div>
-                        <div className={classes.entityListing}>
-                            {filteredEntitys && filteredEntitys.length > 0 ? ( // Use filteredentitys
-                                filteredEntitys.map((entity, index) => (
-                                    <p key={index} onClick={() => clickentityHandler(entity)}>
-                                        {entity.name}
-                                    </p>
-                                ))
-                            ) : (
-                                <p>No matching entitys found.</p> // Show message if no matches
-                            )}
-                        </div>
+            <div className={`${classes.anchor} ${isExpanded ? "" : classes.noDisplay}`}>
+                <div className={classes.dropDownContent} style={style}>
+                    <p>All entitys</p>
+                    <div className={classes.separatorH}></div>
+                    <div className={classes.entityListing}>
+                        {filteredEntitys && filteredEntitys.length > 0 ? ( // Use filteredentitys
+                            filteredEntitys.map((entity, index) => (
+                                <p key={index} onClick={() => clickentityHandler(entity)}>
+                                    {entity.name}
+                                </p>
+                            ))
+                        ) : (
+                            <p>No matching entitys found.</p> // Show message if no matches
+                        )}
                     </div>
                 </div>
-            )}
+            </div>
         </div>
     );
 };
