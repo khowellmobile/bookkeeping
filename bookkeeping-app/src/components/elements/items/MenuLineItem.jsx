02d6c177fa5593a35eb5748c9fb292b6b35f46b2
khowellmobile <khowellmobile@gmail.com>
2025-06-03 13:20:24 -0400

    Added logout button on bottom of menu

--- bookkeeping-app/src/components/elements/items/MenuLineItem.jsx
+++ bookkeeping-app/src/components/elements/items/MenuLineItem.jsx
@@ -2,9 +2,9 @@ import classes from "./MenuLineItem.module.css";
 
 import { Link } from "react-router-dom";
 
-const MenuLineItem = ({ itemName, link = null, icon = null }) => {
+const MenuLineItem = ({ itemName, link = null, icon = null, onClick = null }) => {
     return (
-        <Link to={link} className={classes.mainContainer}>
+        <Link to={link} className={classes.mainContainer} onClick={onClick}>
             <div className={classes.logoContainer}>{icon && <div className={classes.icon}>{icon}</div>}</div>
             <div className={classes.nameContainer}>
                 <p>{itemName}</p>
--- bookkeeping-app/src/components/sections/Menu.jsx
+++ bookkeeping-app/src/components/sections/Menu.jsx
@@ -1,5 +1,8 @@
 import classes from "./Menu.module.css";
 
+import { useContext } from "react";
+import BkpgContext from "../contexts/BkpgContext";
+
 import AccountsIcon from "../../assets/accounts-icon.svg";
 import EntitiesIcon from "../../assets/entities-icon.svg";
 import DashIcon from "../../assets/dashboard-icon.svg";
@@ -13,6 +16,8 @@ import UserIcon from "../../assets/user-icon-white.svg";
 import MenuLineItem from "../elements/items/MenuLineItem";
 
 const Menu = () => {
+    const { logoutUser } = useContext(BkpgContext);
+
     return (
         <div className={classes.mainContainer}>
             <section className={classes.logoSection}>
@@ -24,7 +29,11 @@ const Menu = () => {
             </section>
             <section className={classes.itemsSection}>
                 <MenuLineItem itemName="Dashboard" link="/home" icon={<img src={DashIcon} alt="Icon" />} />
-                <MenuLineItem itemName="Transactions" link="/transactions" icon={<img src={TransactionIcon} alt="Icon" />} />
+                <MenuLineItem
+                    itemName="Transactions"
+                    link="/transactions"
+                    icon={<img src={TransactionIcon} alt="Icon" />}
+                />
                 <MenuLineItem itemName="Accounts" link="/accounts" icon={<img src={AccountsIcon} alt="Icon" />} />
                 <MenuLineItem itemName="Entities" link="/entities" icon={<img src={EntitiesIcon} alt="Icon" />} />
                 <MenuLineItem itemName="Journals" link="/journals" icon={<img src={JournalIcon} alt="Icon" />} />
@@ -33,14 +42,7 @@ const Menu = () => {
                 <MenuLineItem itemName="Support" link="/support" icon={<img src={SupportIcon} alt="Icon" />} />
                 <MenuLineItem itemName="Settings" link="/settings" icon={<img src={SettingsIcon} alt="Icon" />} />
             </section>
-            {/* <section className={classes.profileSection}>
-                <div className={classes.profilePicture}></div>
-                <div className={classes.profileInfo}>
-                    <p className={classes.profileName}>John Doe</p>
-                    <p className={classes.profileDate}>August 3rd, 2024 10:32am</p>
-                </div>
-            </section> */}
-            <MenuLineItem className={classes.logoutItem} itemName="Logout" icon={<img src={UserIcon} alt="Icon" />} />
+            <MenuLineItem className={classes.logoutItem} itemName="Logout" link="/" icon={<img src={UserIcon} alt="Icon" />} onClick={logoutUser}/>
         </div>
     );
 };
