0f17738bfd6afcc8141b215dc6b19f4feb7036e0
khowellmobile <khowellmobile@gmail.com>
2025-06-03 15:16:11 -0400

    Added confirmation modal to AddTransactionsModal

--- bookkeeping-app/src/components/elements/modals/AddTransactionsModal.jsx
+++ bookkeeping-app/src/components/elements/modals/AddTransactionsModal.jsx
@@ -3,18 +3,20 @@ import classes from "./AddTransactionsModal.module.css";
 import { TransactionEntryItem } from "../items/InputEntryItems";
 import AccountDropdown from "../dropdowns/AccountDropdown";
 import { useState, useCallback, useRef } from "react";
+import ConfirmationModal from "./ConfirmationModal";
 
 const AddTransactionsModal = ({ ctxActiveAccount, setPageTrans, handleCloseModal }) => {
     const scrollRef = useRef();
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [activeAccount, setActiveAccount] = useState(ctxActiveAccount);
     const [transactionItems, setTransactionItems] = useState(
         Array(14)
             .fill(null)
             .map(() => ({
                 date: "",
-                entity_id: "",
-                account_id: "",
+                entity: "",
+                account: "",
                 memo: "",
                 amount: "",
                 is_reconciled: false,
@@ -63,6 +65,35 @@ const AddTransactionsModal = ({ ctxActiveAccount, setPageTrans, handleCloseModal
         handleCloseModal();
     };
 
+    const handleCancelClose = () => {
+        const allItemsAreEmpty = transactionItems.every((item) => {
+            // Check if all string properties are empty and is_reconciled is false for this item
+            return (
+                item.date === "" &&
+                item.entity === "" &&
+                item.account === "" &&
+                item.memo === "" &&
+                (item.amount === "" || item.amount == 0) &&
+                item.is_reconciled === false
+            );
+        });
+
+        if (!allItemsAreEmpty) {
+            setIsModalOpen(true);
+        } else {
+            handleCloseModal();
+        }
+    };
+
+    const onConfirm = () => {
+        setIsModalOpen(false);
+        handleCloseModal();
+    };
+
+    const onCancel = () => {
+        setIsModalOpen(false);
+    };
+
     const addTransactions = async (transactionsToAdd) => {
         const accessToken = localStorage.getItem("accessToken");
 
@@ -100,49 +131,62 @@ const AddTransactionsModal = ({ ctxActiveAccount, setPageTrans, handleCloseModal
     };
 
     return (
-        <div className={classes.modalOverlay}>
-            <div className={classes.mainContainer}>
-                <section className={classes.tools}>
-                    <AccountDropdown initalVal={activeAccount} onChange={setActiveAccount} />
-                    <p>What else?</p>
-                    <section className={classes.buttons}>
-                        <button onClick={handleSaveClose}>Save & Close</button>
-                        <button onClick={handleCloseModal}>Cancel</button>
+        <>
+            {isModalOpen && (
+                <ConfirmationModal
+                    text={{
+                        msg: "You are above to leave without saving.",
+                        confirm_txt: "Leave",
+                        cancel_txt: "Stay",
+                    }}
+                    onConfirm={onConfirm}
+                    onCancel={onCancel}
+                />
+            )}
+            <div className={classes.modalOverlay}>
+                <div className={classes.mainContainer}>
+                    <section className={classes.tools}>
+                        <AccountDropdown initalVal={activeAccount} onChange={setActiveAccount} />
+                        <p>What else?</p>
+                        <section className={classes.buttons}>
+                            <button onClick={handleSaveClose}>Save & Close</button>
+                            <button onClick={handleCancelClose}>Cancel</button>
+                        </section>
                     </section>
-                </section>
-                <div className={classes.entryContainer}>
-                    <section className={`${classes.columnNames} ${classes.entryGridTemplate}`}>
-                        <div>
-                            <p>Date</p>
-                        </div>
-                        <div>
-                            <p>Payee</p>
-                        </div>
-                        <div>
-                            <p>Account</p>
-                        </div>
-                        <div>
-                            <p>Memo</p>
-                        </div>
-                        <div>
-                            <p>Amount</p>
-                        </div>
-                    </section>
-                    <section className={classes.items} ref={scrollRef}>
-                        {transactionItems.map((transaction, index) => (
-                            <TransactionEntryItem
-                                vals={transaction}
-                                key={index}
-                                index={index}
-                                onFocus={() => handleFocusLastItem(index)}
-                                onItemChange={handleItemChange}
-                                scrollRef={scrollRef}
-                            />
-                        ))}
-                    </section>
+                    <div className={classes.entryContainer}>
+                        <section className={`${classes.columnNames} ${classes.entryGridTemplate}`}>
+                            <div>
+                                <p>Date</p>
+                            </div>
+                            <div>
+                                <p>Payee</p>
+                            </div>
+                            <div>
+                                <p>Account</p>
+                            </div>
+                            <div>
+                                <p>Memo</p>
+                            </div>
+                            <div>
+                                <p>Amount</p>
+                            </div>
+                        </section>
+                        <section className={classes.items} ref={scrollRef}>
+                            {transactionItems.map((transaction, index) => (
+                                <TransactionEntryItem
+                                    vals={transaction}
+                                    key={index}
+                                    index={index}
+                                    onFocus={() => handleFocusLastItem(index)}
+                                    onItemChange={handleItemChange}
+                                    scrollRef={scrollRef}
+                                />
+                            ))}
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
