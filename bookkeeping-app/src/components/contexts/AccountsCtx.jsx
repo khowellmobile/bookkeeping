b01ed15f323a463ea96a32364dc93e1a6f27f7ca
khowellmobile <khowellmobile@gmail.com>
2025-06-04 15:30:14 -0400

    Moved all account related requests to AccountsCtx

--- bookkeeping-app/src/components/contexts/AccountsCtx.jsx
+++ bookkeeping-app/src/components/contexts/AccountsCtx.jsx
@@ -8,6 +8,9 @@ const AccountsCtx = createContext({
     populateCtxAccounts: () => {},
     setCtxActiveAccount: () => {},
     setCtxAccountList: () => {},
+    ctxAddAccount: () => {},
+    ctxUpdateAccount: () => {},
+    ctxDeleteAccount: () => {},
 });
 
 export function AccountsCtxProvider(props) {
@@ -38,12 +41,85 @@ export function AccountsCtxProvider(props) {
         }
     };
 
+    const ctxAddAccount = async (account) => {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/accounts/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${ctxAccessToken}`,
+                },
+                body: JSON.stringify(account),
+            });
+
+            const newAccount = await response.json();
+            setCtxAccountList((prev) => [...prev, newAccount]);
+        } catch (error) {
+            console.error("Error sending Account Info:", error);
+        }
+    };
+
+    const ctxUpdateAccount = async (editedAccount) => {
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/api/accounts/${editedAccount.id}/`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${ctxAccessToken}`,
+                },
+                body: JSON.stringify(editedAccount),
+            });
+
+            if (!response.ok) {
+                console.log("Error:", response.error);
+                return;
+            }
+
+            const updatedData = await response.json();
+
+            setCtxAccountList((prevAccounts) =>
+                prevAccounts.map((acc) => {
+                    if (acc.id === editedAccount.id) {
+                        return updatedData;
+                    } else {
+                        return acc;
+                    }
+                })
+            );
+        } catch (error) {
+            console.error("Error editing account:", error);
+        }
+    };
+
+    const ctxDeleteAccount = async (accountId) => {
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/api/accounts/${accountId}/`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${ctxAccessToken}`,
+                },
+                body: JSON.stringify({ is_deleted: true }),
+            });
+
+            if (!response.ok) {
+                console.log("Error:", response.error);
+                return;
+            }
+        } catch (error) {
+            console.error("Error marking account inactive:", error);
+        }
+    };
+
     const context = {
         ctxActiveAccount,
         ctxAccountList,
         populateCtxAccounts,
         setCtxActiveAccount,
         setCtxAccountList,
+        ctxAddAccount,
+        ctxUpdateAccount,
+        ctxDeleteAccount,
     };
 
     return <AccountsCtx.Provider value={context}>{props.children}</AccountsCtx.Provider>;
