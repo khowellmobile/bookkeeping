24e05c24991f1f0862e671e3ceeb9aa6d9644b44
khowellmobile <khowellmobile@gmail.com>
2025-06-12 08:57:56 -0400

    Changed account to use one inputFields state variable instead of 5 state variables

--- bookkeeping-app/src/components/elements/modals/AddAccountModal.jsx
+++ bookkeeping-app/src/components/elements/modals/AddAccountModal.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./AddAccountModal.module.css";
 
 import AccountsCtx from "../../contexts/AccountsCtx";
@@ -8,12 +8,14 @@ import downChevIcon from "../../../assets/chevron-down-icon.svg";
 
 const AddAccountModal = ({ handleCloseModal }) => {
     const { ctxAddAccount } = useContext(AccountsCtx);
+    const [inputFields, setInputFields] = useState({
+        name: "",
+        type: "",
+        initial_balance: "",
+        description: "",
+        account_number: "",
+    });
 
-    const [accountName, setAccountName] = useState("");
-    const [accountNumber, setAccountNumber] = useState("");
-    const [accountType, setAccountType] = useState("");
-    const [initialBalance, setInitialBalance] = useState("");
-    const [accountDescription, setAccountDescription] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const [isExpanded, setIsExpanded] = useState(false);
@@ -27,28 +29,36 @@ const AddAccountModal = ({ handleCloseModal }) => {
 
     const addAccount = async () => {
         const accountToAdd = {
-            name: accountName,
-            type: accountType.toLowerCase(),
-            initial_balance: initialBalance,
-            description: accountDescription,
-            account_number: accountNumber,
+            ...inputFields,
+            type: inputFields.type.toLowerCase(),
         };
 
         ctxAddAccount(accountToAdd);
     };
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setInputFields((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
     const clickTypeHandler = (type) => {
-        setAccountType(type);
+        setInputFields((prev) => ({
+            ...prev,
+            type: type,
+        }));
         setIsExpanded(false);
     };
 
     const handleCancelClose = () => {
         if (
-            accountName !== "" ||
-            accountNumber !== "" ||
-            accountType !== "" ||
-            initialBalance !== "" ||
-            accountDescription !== ""
+            inputFields.name !== "" ||
+            inputFields.account_number !== "" ||
+            inputFields.type !== "" ||
+            inputFields.initial_balance !== "" ||
+            inputFields.description !== ""
         ) {
             setIsModalOpen(true);
         } else {
@@ -87,25 +97,27 @@ const AddAccountModal = ({ handleCloseModal }) => {
                             <p className={classes.label}>Account Name</p>
                             <input
                                 type="text"
+                                name="name"
                                 placeholder="Enter account name (e.g., Checking, Savings, Credit Card)"
-                                value={accountName}
-                                onChange={(e) => setAccountName(e.target.value)}
+                                value={inputFields.name}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className={classes.inputCluster}>
                             <p className={classes.label}>Account Number</p>
                             <input
                                 type="text"
+                                name="account_number"
                                 placeholder="Enter account number (optional)"
-                                value={accountNumber}
-                                onChange={(e) => setAccountNumber(e.target.value)}
+                                value={inputFields.account_number}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className={classes.inputCluster}>
                             <p className={classes.label}>Account Type</p>
                             <div className={classes.accountTypeDiv} onClick={() => setIsExpanded((prev) => !prev)}>
-                                {accountType ? (
-                                    <p>{accountType}</p>
+                                {inputFields?.type ? (
+                                    <p>{inputFields.type}</p>
                                 ) : (
                                     <p className={classes.placeholder}>Select account type</p>
                                 )}
@@ -128,14 +140,16 @@ const AddAccountModal = ({ handleCloseModal }) => {
                             <p className={classes.label}>Initial Balance</p>
                             <input
                                 type="text"
+                                name="initial_balance"
                                 placeholder="Enter starting balance (e.g., 0.00)"
-                                value={initialBalance}
-                                onChange={(e) => setInitialBalance(e.target.value)}
+                                value={inputFields.initial_balance}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <textarea
-                            value={accountDescription}
-                            onChange={(e) => setAccountDescription(e.target.value)}
+                            value={inputFields.description}
+                            name="description"
+                            onChange={handleInputChange}
                         ></textarea>
                     </section>
                     <section className={classes.buttons}>
