b0d0629925b0c419e404f6b8610eff69c260dc72
khowellmobile <khowellmobile@gmail.com>
2025-07-07 08:15:04 -0400

    Adding expanded rent content

--- bookkeeping-app/src/components/elements/items/RentItem.jsx
+++ bookkeeping-app/src/components/elements/items/RentItem.jsx
@@ -1,9 +1,30 @@
 import { useEffect, useState } from "react";
 import classes from "./RentItem.module.css";
+import AddInputCluster from "../misc/AddInputCluster";
 
 const RentItem = ({ item }) => {
     const [isClicked, setIsClicked] = useState(false);
     const [isAbsolute, setIsAbsolute] = useState(false);
+    const [inputFields, setInputFields] = useState({
+        amount: item.amount,
+        payee: "John Michals",
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === "isRecurring") {
+            setInputFields((prev) => ({
+                ...prev,
+                [name]: e.target.checked,
+            }));
+        } else {
+            setInputFields((prev) => ({
+                ...prev,
+                [name]: value,
+            }));
+        }
+    };
 
     const handleClick = () => {
         setIsClicked((prev) => !prev);
@@ -38,6 +59,20 @@ const RentItem = ({ item }) => {
                     <div className={`${classes.header} ${isClicked && classes[item.status]}`}>
                         <div className={`${classes.statIndicator} ${classes[item.status]}`}></div>
                         <p>{item.title}</p>
+                    </div>
+                    <div className={`${classes.rentInfo} ${!isClicked && classes.noDisplay}`}>
+                        <div className={classes.inputCluster}>
+                            <p className={classes.label}>Amount</p>
+                            <input type="text" name="amount" value={inputFields.amount} onChange={handleInputChange} />
+                        </div>
+                        <div className={classes.inputCluster}>
+                            <p className={classes.label}>Payee</p>
+                            <input type="text" name="payee" value={inputFields.payee} onChange={handleInputChange} />
+                        </div>
+                        <div className={classes.inputCluster}>
+                            {/* <p className={classes.label}>Payee</p> */}
+                            <textarea name="description" value={inputFields.payee} onChange={handleInputChange} />
+                        </div>
                     </div>
                 </div>
             </div>
