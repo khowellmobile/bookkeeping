f5eb3ff94d3d09b4ceb79a73590be3032eaafa8b
khowellmobile <khowellmobile@gmail.com>
2025-07-24 11:29:23 -0400

    Added email to layout listing of users email

--- bookkeeping-app/src/components/layout/Layout.jsx
+++ bookkeeping-app/src/components/layout/Layout.jsx
@@ -1,12 +1,16 @@
+import { useContext } from "react";
+
 import classes from "./Layout.module.css";
 
+import AuthCtx from "../contexts/AuthCtx";
 import Header from "../sections/Header";
 import Menu from "../sections/Menu";
-
 import NotificationIcon from "../../assets/notification-icon.svg";
 import UserIcon from "../../assets/user-icon-black.svg";
 
 const Layout = (props) => {
+    const { ctxUserData } = useContext(AuthCtx);
+
     return (
         <div className={classes.layoutContainer}>
             <div className={classes.menuContainer}>
@@ -20,7 +24,9 @@ const Layout = (props) => {
                             <img className={classes.icon} src={NotificationIcon} alt="Icon" />
                         </div>
                         <div>
-                            <p className={classes.userEmail}>Exampleemail@gmail.com</p>
+                            <p className={classes.userEmail}>
+                                {ctxUserData?.email.charAt(0).toUpperCase() + ctxUserData?.email.slice(1)}
+                            </p>
                             <img className={classes.icon} src={UserIcon} alt="Icon" />
                         </div>
                     </div>
