7cb2ecd8068859972101144829b3fd126c2a3ff5
khowellmobile <khowellmobile@gmail.com>
2026-06-17 09:25:37 -0400

    Fixed CORS issue

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,6 +131,11 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
